c073963de75ab9a348c5718499b92a02d880eb88
Yanan LYU <arol666@hotmail.com>
2024-10-11 23:37:03 +0200

    feat: complete layout of page projects

--- pages/Projects.vue
+++ pages/Projects.vue
@@ -1,10 +1,18 @@
 <template>
-    <div>
-        <ul>
-            <li>
-                <h3>Listy</h3>
-                <span>A simple task tracker for daily use</span>
-            </li>
-        </ul>
+    <div class="p-5 text-sm space-y-4 w-max">
+        <NuxtLink class="flex gap-5 border rounded-md p-5 hover:bg-neutral-50" to="https://listy-ecru.vercel.app/">
+            <Icon name="mdi:list-box-outline" class="my-auto" mode="svg" size="2em"></Icon>
+            <div>
+                <div class="text-lg">Listy</div>
+                <span>A simple task tracker for daily use</span>                
+            </div>
+        </NuxtLink>
+        <NuxtLink class="flex gap-5 border rounded-md p-5 hover:bg-neutral-50" to="https://github.com/lyuya/yanan">
+            <Icon name="mdi:robot-happy-outline" class="my-auto" mode="svg" size="2em"></Icon>
+            <div>
+                <div class="text-lg">yanan.me</div>
+                <span>My personal website</span>            
+            </div>
+        </NuxtLink>
     </div>
 </template>
