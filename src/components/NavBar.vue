5918028e82b87aa1b1d1e7d829f1099ff2f6d58a
Yanan LYU <arol666@hotmail.com>
2024-10-31 18:40:25 +0100

    feat: remove resume tab

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,7 +9,6 @@ const activeStyle = 'opacity-100 underline';
     <header class="flex justify-between font-bold gap-8">
         <div class="p-3 sm:h-full flex gap-5">
             <NuxtLink to="/" :class="route.path === '/' ? activeStyle : inactiveStyle">Home</NuxtLink>
-            <NuxtLink to="/resume" :class="route.path === '/resume' ? activeStyle : inactiveStyle">Resume</NuxtLink>
             <NuxtLink to="/projects" :class="route.path === '/projects' ? activeStyle : inactiveStyle">Projects
             </NuxtLink>
         </div>
